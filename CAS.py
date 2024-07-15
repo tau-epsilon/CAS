754c02659e732761aba733600783cbd729b6b115
tarmak4317 <167742622+tarmak4317@users.noreply.github.com>
2024-07-16 01:08:38 +0530

    Add files via upload

--- CAS.py
+++ CAS.py
@@ -4,7 +4,7 @@ class Symbol:
         self.pre = pre
         self.post = post
 
-    def __truediv__(self, other):
+    def __div__(self, other):
         return Symbol(self.var, self.pre / other.pre, self.post - other.post)
 
     def __mul__(self, other):
@@ -18,27 +18,48 @@ class Symbol:
 
     def __str__(self):
         return(str(self.pre)+self.var+str(self.post))
-
-
-def is_operator(obj):
-    operators = "+-*/%<>=!&|^~"
-    if obj in operators:
-        return True
-    return False
-
-arr = [Symbol('x', 2, 3), '*', Symbol('x', 3, 4)]
-arr2 = []
-for x in arr:
-    y = arr.index(x)
-    if not isinstance(x,Symbol):
-        if x == '/':
-            arr2.append(arr[y - 1] / arr[y + 1])
-        elif x == '*':
-            arr2.append(arr[y - 1] * arr[y + 1])
-        elif x == '+':
-            arr2.append(arr[y - 1] + arr[y + 1])
-        elif x == '-':
-            arr2.append(arr[y - 1] - arr[y + 1])
-
-for z in arr2:
-    print(z)
+def remover(arr):
+    while(True):
+        if "brownMunde" in arr:
+            arr.remove("brownMunde")
+        else:
+            break
+arr=[Symbol('x',3,2),'*',Symbol('x',3,2),'-',Symbol('x',1,3)]
+x=0
+while(x<len(arr)):
+    if arr[x]=='/':
+        arr[x+1]=arr[x-1]/arr[x+1]
+        arr[x-1]="brownMunde"
+        arr[x]="brownMunde"
+        remover(arr)
+    x+=1
+x=0
+while(x<len(arr)):
+    if arr[x]=='*':
+        arr[x+1]=arr[x-1]*arr[x+1]
+        arr[x-1]="brownMunde"
+        arr[x]="brownMunde"
+        remover(arr)
+    x+=1
+x=0
+while(x<len(arr)):
+    if arr[x]=='+':
+        if arr[x+1].post==arr[x-1].post:
+            arr[x+1]=arr[x-1]+arr[x+1]
+            arr[x-1]="brownMunde"
+            arr[x]="brownMunde"
+            remover(arr)
+    x+=1
+x=0
+while(x<len(arr)):
+    if arr[x]=='-':
+        if arr[x+1].post==arr[x-1].post:
+            arr[x+1]=arr[x-1]-arr[x+1]
+            arr[x-1]="brownMunde"
+            arr[x]="brownMunde"
+            remover(arr)
+    x+=1
+i=0
+while(i<len(arr)):
+    print(arr[i],end='')
+    i+=1
