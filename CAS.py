20db5c49aa3bcf16b9c8b1e4d46e3c693e1de2dd
tarmak4317 <167742622+tarmak4317@users.noreply.github.com>
2024-07-19 11:32:41 +0530

    Add files via upload

--- CAS.py
+++ CAS.py
@@ -22,24 +22,41 @@ class Frac:
     def __init__(self,num,den):
         self.num=num
         self.den=den
+    def converter(obj):
+        if isinstance(obj,Symbol):
+            return Frac(obj,Symbol('x',1,0))
+        else:
+            return obj
     def __truediv__(self,other):
+        self=Frac.converter(self)
+        other=Frac.converter(other)
         return Frac(self.num*other.den,self.den*other.num)
     def __mul__(self,other):
+        self=Frac.converter(self)
+        other=Frac.converter(other)
         return Frac(self.num*other.num,self.den*other.den)
     def __add__(self,other):
+        self=Frac.converter(self)
+        other=Frac.converter(other)
         if self.den==other.den:
-            num=self.num+other.num
+            num=[self.num,'+',other.num]
+            solver(num)
             den=self.den
         else:
-            num=(self.num*other.den)+(other.num*self.den)
+            num=[self.num*other.den,'+',other.num*self.den]
+            solver(num)
             den=self.den*other.den
         return Frac(num,den)
     def __sub__(self,other):
+        self=Frac.converter(self)
+        other=Frac.converter(other)
         if self.den==other.den:
-            num=self.num-other.num
+            num=[self.num,'-',other.num]
+            solver(num)
             den=self.den
         else:
-            num=(self.num*other.den)-(other.num*self.den)
+            num=[self.num*other.den,'-',other.num*self.den]
+            solver(num)
             den=self.den*other.den
         return Frac(num,den)
     def __str__(self):
@@ -50,42 +67,65 @@ def remover(arr):
             arr.remove("brownMunde")
         else:
             break
-arr=[Frac(Symbol('x',3,2),Symbol('x',2,1)),'*',Frac(Symbol('x',3,2),Symbol('x',3,5))]
-x=0
-while(x<len(arr)):
-    if arr[x]=='/':
-        arr[x+1]=arr[x-1]/arr[x+1]
-        arr[x-1]="brownMunde"
-        arr[x]="brownMunde"
-        remover(arr)
-    x+=1
-x=0
-while(x<len(arr)):
-    if arr[x]=='*':
-        arr[x+1]=arr[x-1]*arr[x+1]
-        arr[x-1]="brownMunde"
-        arr[x]="brownMunde"
-        remover(arr)
-    x+=1
-x=0
-while(x<len(arr)):
-    if arr[x]=='+':
-        if arr[x+1].post==arr[x-1].post:
-            arr[x+1]=arr[x-1]+arr[x+1]
+def solver(arr):
+    x=0
+    while(x<len(arr)):
+        if arr[x]=='/':
+            arr[x+1]=arr[x-1]/arr[x+1]
             arr[x-1]="brownMunde"
             arr[x]="brownMunde"
             remover(arr)
-    x+=1
-x=0
-while(x<len(arr)):
-    if arr[x]=='-':
-        if arr[x+1].post==arr[x-1].post:
-            arr[x+1]=arr[x-1]-arr[x+1]
+        x+=1
+    x=0
+    while(x<len(arr)):
+        if arr[x]=='*':
+            arr[x+1]=arr[x-1]*arr[x+1]
             arr[x-1]="brownMunde"
             arr[x]="brownMunde"
             remover(arr)
-    x+=1
+        x+=1
+    x=0
+    while(x<len(arr)):
+        if arr[x]=='+':
+            if isinstance(arr[x+1],Symbol) and isinstance(arr[x-1],Symbol):
+                if arr[x+1].post==arr[x-1].post:
+                    arr[x+1]=arr[x-1]+arr[x+1]
+                    arr[x-1]="brownMunde"
+                    arr[x]="brownMunde"
+                    remover(arr)
+            else:
+                arr[x+1]=arr[x-1]+arr[x+1]
+                arr[x-1]="brownMunde"
+                arr[x]="brownMunde"
+                remover(arr)
+        x+=1
+    x=0
+    while(x<len(arr)):
+        if arr[x]=='-':
+            if isinstance(arr[x+1],Symbol) and isinstance(arr[x-1],Symbol):
+                if arr[x+1].post==arr[x-1].post:
+                    arr[x+1]=arr[x-1]-arr[x+1]
+                    arr[x-1]="brownMunde"
+                    arr[x]="brownMunde"
+                    remover(arr)
+            else:
+                arr[x+1]=arr[x-1]-arr[x+1]
+                arr[x-1]="brownMunde"
+                arr[x]="brownMunde"
+                remover(arr)
+        x+=1
+def printer(a):
+    j=0
+    while(i<len(a)):
+        print(a[j],end='')
+        j+=1
+
+arr=[Frac(Symbol('x',3,2),Symbol('x',2,1)),'-',Symbol('x',3,2)]
+solver(arr)
 i=0
 while(i<len(arr)):
-    print(arr[i],end='')
+    if isinstance(arr[i],list):
+        printer(arr[i])
+    else:
+        print(arr[i],end='')
     i+=1
